fe6e892a06253df361e10edc27b9ef513968f31a
zpawn <zpawn@zpawn-mac.local>
2022-09-11 14:25:34 +0300

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require("fs/promises");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+const _ = require("lodash");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const rootHandler = async (req, res) => {
+    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    const data = {
+        url,
+        body: req.body,
+        method: req.method,
+    }
+    console.log(">>> req:", data);
+    await fs.writeFile("req.json", JSON.stringify(data, null, 2));
+};
+
+app.get("/", (req, res) => {
+    rootHandler(req, res);
+    res.status(201).json({ success: true });
+});
+
+app.post("/", (req, res) => {
+    rootHandler(req, res);
+    res.status(201).json({ success: true });
+});
+
+app.listen(3000, () => console.log("API Server running..."));
